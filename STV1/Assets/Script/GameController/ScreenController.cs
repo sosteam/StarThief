9bc5ad9cfe87e904ea4b1c27e015f5ffdece1554
sosteam <sosteam@gmail.com>
2018-03-23 18:16:28 +0900

    confirm

---
+++ STV1/Assets/Script/GameController/ScreenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenController : MonoBehaviour 
+{
+
+    void Awake()
+    { 
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(540, 960, true); // true는 전체화면옵션
+    }
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
