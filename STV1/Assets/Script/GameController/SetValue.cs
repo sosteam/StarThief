b004e11e5d16efc61ddcb4a42dfc4520bde45aa4
sosteam <sosteam@gmail.com>
2018-03-26 07:38:22 +0900

    recommit

---
+++ STV1/Assets/Script/GameController/SetValue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GlobalSpeed
+{
+	public float PlayerMove;
+	public float PlayerRotate;
+}
+
+public class SetValue : MonoBehaviour 
+{
+	public GlobalSpeed globalSpeed;
+
+}
---
+++ STV1/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	float playerSpeedMove;
+	float playerSpeedRotate;
+	Rigidbody2D rb;
+	// Use this for initialization
+	Vector2 v2 = new Vector2(45, 45);  //방향  target.position - player.position;
+	Vector2 playerVelocity;
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		SetValue setValue = GameObject.Find("SetValue").GetComponent<SetValue>(); 
+		playerSpeedMove = setValue.globalSpeed.PlayerMove;
+		playerSpeedRotate = setValue.globalSpeed.PlayerRotate;
+
+		movePlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void FixedUpdate() 
+	{
+		
+	}
+
+	void movePlayer()
+	{
+		 rb.AddForce( new Vector2(45.0f,45.0f) * playerSpeedMove);
+	}
+}
