628f828c3dcc40ea75fd1ccccda296b549aa6985
sosteam <sosteam@gmail.com>
2018-03-28 05:51:54 +0900

    222

--- STV1/Assets/Script/Player/PlayerController.cs
+++ STV1/Assets/Script/Player/PlayerController.cs
@@ -33,6 +33,9 @@ public class PlayerController : MonoBehaviour
 
 			movePlayer( vDirection);
 		}
+
+		//Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
 	}
 
 	void FixedUpdate() 
